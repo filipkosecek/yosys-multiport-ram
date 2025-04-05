c29980b3537fb6003d1f88d1fa431a51aa30452a
Filip Kosecek <filip.kosecek@gmail.com>
2025-04-05 12:09:00 +0200

    fix a typo in 4port_ram_large.v

--- 4port_ram_large.v
+++ 4port_ram_large.v
@@ -9,7 +9,7 @@ module ram (
 	output reg [WORD_SIZE - 1:0] rdata1, rdata2, rdata3, rdata4
 );
 
-localparam RAM_SIZE = 32;
+localparam RAM_SIZE = 256;
 localparam WORD_SIZE = 8;
 
 (* no_rw_check *)
